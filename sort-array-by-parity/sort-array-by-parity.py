23a130524876c48d5dc1f43177af77d7a79d53ac
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2022-01-04 03:48:44 -0500

    Time: 116 ms, Memory: 14.9 MB -LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+              
+        beg, end = 0, len(nums) - 1
+        
+        while beg <= end:
+            if nums[beg] % 2 == 0:
+                beg += 1                
+            else:
+                nums.append(nums.pop(beg))
+                end -= 1
+        return nums
