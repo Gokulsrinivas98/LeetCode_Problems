d0d0f32d8418239b2305f507c28dffd09c893d38
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2022-01-05 23:11:20 -0500

    Time: 587 ms (5.19%), Space: 14.9 MB (18.85%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        #capacity -> no of seats
+        # trip[i] = [numPassengersi, fromi, toi]
+        m = max([trips[i][2] for i in range(len(trips))])
+        arr = [0] * m
+        for i in trips:
+            for j in range(i[1],i[2],1):
+                arr[j] += i[0]
+                if arr[j] > capacity:
+                    return False
+        return True
+        
