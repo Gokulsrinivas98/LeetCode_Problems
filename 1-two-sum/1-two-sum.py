440301b5f4cbc55bc3e0545d98bdc76f1b6ed4e7
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2022-01-06 00:46:24 -0500

    Time: 110 ms (42.13%), Space: 15.4 MB (41.34%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #hashmap
+        seen = {}
+        for i, b in enumerate(nums):
+            # a + b = target -> a = target - b
+            a = target - b
+            if a in seen:
+                return [seen[a], i]
+            seen[b] = i
+        
