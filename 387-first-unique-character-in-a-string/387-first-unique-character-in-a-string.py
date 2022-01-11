9b9355b8d4ae867320818a8db9ac72ee682b51ba
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2022-01-11 17:02:42 -0500

    Time: 66 ms (95.54%), Space: 14.4 MB (70.96%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,7 +1,9 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        for str in s:
-            if s.count(str) == 1:
-                return s.index(str)
-        return -1
-        
+#         for str in s:
+#             if s.count(str) == 1:
+#                 return s.index(str)
+#         return -1
+        alp = 'abcdefghijklmnopqrstuvwxyz'
+        ind = [s.index(char) for char in alp if s.count(char) == 1]
+        return min(ind) if len(ind) > 0 else -1
