ba3f4fe7351987535c46e467b7064dae6dc78fdf
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-01-23 07:17:47 +0530

    Time: 45 ms (99.35%), Space: 13.9 MB (26.01%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,7 +1,9 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        l=[]
-        for i in range(0,len(accounts)):
-           l.append(sum(accounts[i]))
-        return max(l)
+        # l=[]
+        # for i in range(0,len(accounts)):
+        #    l.append(sum(accounts[i]))
+        # return max(l)
+        
+        return max(map(sum, accounts))
         
