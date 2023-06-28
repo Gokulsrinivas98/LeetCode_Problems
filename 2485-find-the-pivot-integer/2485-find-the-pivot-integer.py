1625283ec69de9040c9d3a4fb331782136bde19e
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-06-27 21:19:07 -0400

    Time: 913 ms (13.96%), Space: 16.3 MB (69.57%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,8 +1,6 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        elements = []
-        for  i in range(1,n+1):
-            elements.append(i)
+        elements = list(range(1, n+1))
         for i in range(n):
             if sum(elements[:i+1]) == sum(elements[i:]):
                 return i+1
