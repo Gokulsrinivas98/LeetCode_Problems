00c39a1cc33daf6417ee3ab4c5ea21c99f2189d5
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-06-29 01:30:22 -0400

    Time: 617 ms (79.00%), Space: 19.6 MB (91.32%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,12 +1,28 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        maxi = 0
-        for i in range(len(words)):
-            chars_i = set(words[i])
-            for j in range(i+1,len(words)):
-                if not chars_i.intersection(set(words[j])):
-                # if set(words[i]).isdisjoint(set(words[j])):
-                    count = len(words[i])*len(words[j])
-                    maxi= max(count,maxi)
+        bitmasks = []
+        for word in words:
+            bitmask = 0
+            for char in word:
+                bitmask |= 1 << (ord(char) - ord('a'))
+            bitmasks.append(bitmask)
+
+        max_product = 0
+        n = len(words)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if bitmasks[i] & bitmasks[j] == 0:
+                    product = len(words[i]) * len(words[j])
+                    max_product = max(max_product, product)
+        
+        return max_product
+#         maxi = 0
+#         for i in range(len(words)):
+#             chars_i = set(words[i])
+#             for j in range(i+1,len(words)):
+#                 if not chars_i.intersection(set(words[j])):
+#                 # if set(words[i]).isdisjoint(set(words[j])):
+#                     count = len(words[i])*len(words[j])
+#                     maxi= max(count,maxi)
                 
-        return maxi
+#         return maxi
