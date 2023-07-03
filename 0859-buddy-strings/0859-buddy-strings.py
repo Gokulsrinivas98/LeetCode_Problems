b13221d36f728a5735b2c25fb90488e74b48aa22
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-07-02 21:20:35 -0400

    Time: 56 ms (25.51%), Space: 16.5 MB (92.04%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -9,6 +9,6 @@ class Solution:
             if s!= g:
                 n_s +=s
                 n_goal +=g
-        if len(n_s) != len(n_goal) or len(n_s) != 2 or len(n_s)==0: return False
-        if n_s[0] != n_goal[1] or n_s[1] != n_goal[0] : return False
+        if len(n_s) != len(n_goal) or len(n_s) != 2 or len(n_s)==0 or n_s[0] != n_goal[1] or n_s[1] != n_goal[0]: return False
+     
         else: return True
