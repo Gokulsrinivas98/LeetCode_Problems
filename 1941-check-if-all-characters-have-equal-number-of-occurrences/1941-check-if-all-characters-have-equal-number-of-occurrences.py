38166d2417515112ad0467791a76728ca81066e3
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-07-03 03:58:32 -0400

    Time: 91 ms (5.32%), Space: 16.3 MB (54.49%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,5 +1,7 @@
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        c1 = Counter(s)
-        lst = [c1[i] for i in c1]        
-        return all(element == lst[0] for element in lst[1:])
+        return len(set([Counter(s)[i] for i in Counter(s)])) == 1
+        # c1 = Counter(s)
+        
+        # lst = [c1[i] for i in c1]        
+        # return all(element == lst[0] for element in lst[1:])
