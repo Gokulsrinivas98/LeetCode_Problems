d443c821369223c10c6211dd842492b72aded987
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-07-04 02:12:40 -0400

    Time: 75 ms (65.30%), Space: 18.3 MB (35.53%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        for i in nums:
-            if nums.count(i) == 1:
-                return i
-        
-        # c = Counter(nums)
-        # for i in c:
-        #     if c[i] !=3:
+        # for i in nums:
+        #     if nums.count(i) == 1:
         #         return i
-        # return -1
+        
+        c = Counter(nums)
+        for i in c:
+            if c[i] !=3:
+                return i
+        return -1
