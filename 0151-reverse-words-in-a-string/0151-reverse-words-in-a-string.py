5c3a18613336232303c3db9b2b3b1b51aad7949b
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-07-06 18:47:21 -0400

    Time: 52 ms (41.01%), Space: 16.5 MB (24.69%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,12 +1,21 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        words = s.split(' ')
         rs = []
-        r = []
-        for i in s:
-            if i != ' ' : 
-                r.append(i)
-            else:
-                rs.insert(0,r)
-                r = []
-        rs.insert(0,r)
-        return ' '.join([''.join(sublist) for sublist in rs if sublist != []])
+        while words:
+            w = words.pop()
+            if w.strip() == '':
+                continue
+            rs.append(w)
+        return ' '.join(rs)
+    
+        # rs = []
+        # r = []
+        # for i in s:
+        #     if i != ' ' : 
+        #         r.append(i)
+        #     else:
+        #         rs.insert(0,r)
+        #         r = []
+        # rs.insert(0,r)
+        # return ' '.join([''.join(sublist) for sublist in rs if sublist != []])
