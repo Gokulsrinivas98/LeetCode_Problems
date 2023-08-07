d121833fb5e25d58d1bb59c745e92f76da1ba83f
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-08-06 20:35:55 -0700

    Time: 220 ms (95.57%), Space: 18 MB (29.17%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        result = duration
+        for i in range(len(timeSeries)-1):
+            result += min(timeSeries[i+1]-timeSeries[i],duration)
+        return result
+        
+        
