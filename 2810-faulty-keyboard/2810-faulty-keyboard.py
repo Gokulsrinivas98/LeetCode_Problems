94f69c8971321c4ec2709022e9f01125c14cc4f5
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-09-10 11:45:11 -0700

    Time: 46 ms (77.41%), Space: 16.3 MB (34.99%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.py
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -1,11 +1,19 @@
 class Solution:
     def finalString(self, s: str) -> str:
-        s = list(s)
-        start = 0
-        while start < len(s):
-            if s[start] == 'i':
-                s[0:start] = s[0:start][::-1]
-                s.remove(s[start])
-                start -=1
-            start +=1
-        return "".join(s)
+        # s = list(s)
+        # start = 0
+        # while start < len(s):
+        #     if s[start] == 'i':
+        #         s[0:start] = s[0:start][::-1]
+        #         s.remove(s[start])
+        #         start -=1
+        #     start +=1
+        # return "".join(s)
+        
+        o = []
+        for c in s:
+            if c != 'i':
+                o.append(c)
+            else:
+                o = o[::-1]
+        return "".join(o)
