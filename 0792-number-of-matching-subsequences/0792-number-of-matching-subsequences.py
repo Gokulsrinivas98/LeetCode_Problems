5fd2f829372c54795dd8ca285cb2331ddc0423b8
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-09-17 14:22:21 -0700

    Time: 244 ms (97.50%), Space: 18.4 MB (84.52%) - LeetHub

--- 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -1,24 +1,43 @@
-class Node:
-    def __init__(self, word):
-        self.word = word
-        self.index = 0
-
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        buckets = defaultdict(list)
+        word_count = {}
         for word in words:
-            startingChar = word[0]
-            buckets[startingChar].append(Node(word))
+            word_count[word] = word_count.get(word, 0) + 1
 
-        ans = 0
-        for c in s:
-            currBucket = buckets[c]
-            buckets[c] = []
-            for node in currBucket:
-                node.index += 1  # Point to next character of node.word
-                if node.index == len(node.word):
-                    ans += 1
-                else:
-                    startingChar = node.word[node.index]
-                    buckets[startingChar].append(node)
-        return ans
+        def is_subsequence(s, word):
+            i = 0
+            for char in word:
+                i = s.find(char, i) + 1  # s.find(char, i) in s[i:], return -1
+                if not i:
+                    return False
+            return True
+
+        res = 0
+        for word in word_count:
+            if is_subsequence(s, word):
+                res += word_count[word]
+        return res
+# class Node:
+#     def __init__(self, word):
+#         self.word = word
+#         self.index = 0
+
+# class Solution:
+#     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+#         buckets = defaultdict(list)
+#         for word in words:
+#             startingChar = word[0]
+#             buckets[startingChar].append(Node(word))
+
+#         ans = 0
+#         for c in s:
+#             currBucket = buckets[c]
+#             buckets[c] = []
+#             for node in currBucket:
+#                 node.index += 1  # Point to next character of node.word
+#                 if node.index == len(node.word):
+#                     ans += 1
+#                 else:
+#                     startingChar = node.word[node.index]
+#                     buckets[startingChar].append(node)
+#         return ans
