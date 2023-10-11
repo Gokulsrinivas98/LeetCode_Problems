3c341c2075a1c8a503dae8dc0947ec19cd3e282f
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-10-10 21:26:43 -0700

    Time: 37 ms (82.00%), Space: 16.4 MB (34.75%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -1,12 +1,20 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        count = 0
-        new_a = ""
-        while len(new_a) <= 10000:
-            if b not in new_a:
-                new_a+=a
-                count+=1
-            else:
-                return count
-        return -1
+        # count = 0
+        # new_a = ""
+        # while len(new_a) <= 10000:
+        #     if b not in new_a:
+        #         new_a+=a
+        #         count+=1
+        #     else:
+        #         return count
+        # return -1
         
+        if len(a) == 0 :
+            return -1
+        rep = math.ceil(len(b)/len(a))
+        for i in range(2):
+            if b in a*rep:
+                return rep
+            rep +=1
+        return -1
