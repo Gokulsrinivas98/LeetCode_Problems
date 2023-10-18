2821a701b451a854ad9548333667a7dc40c903a4
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-10-17 18:23:26 -0700

    Time: 33 ms (87.28%), Space: 16.1 MB (77.75%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,6 +1,10 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        a = [1]
-        for _ in range(rowIndex):
-            a = [x+y for x,y in zip([0]+a,a+[0])]
-        return a
+        prev = 1
+        result = [1]
+        for i in range(1,rowIndex+1):
+            next_val = prev*(rowIndex-i+1)//i
+            result.append(next_val)
+            prev = next_val
+        return result
+        
