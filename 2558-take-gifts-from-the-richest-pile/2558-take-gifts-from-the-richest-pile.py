100f98290316fb37e3b88cd1beac9d77bc3e0589
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-10-26 18:33:44 -0700

    Time: 45 ms (94.10%), Space: 16.5 MB (32.38%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,8 +1,15 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        # for _ in range(k):
+        #     # a = max(gifts)
+        #     # index = gifts.index(a)
+        #     gifts[gifts.index(max(gifts))]= int((max(gifts))**0.5)
+        # return sum(gifts)
+        
+        gifts = [-i for i in gifts]
+        heapify(gifts)
         for _ in range(k):
-            # a = max(gifts)
-            # index = gifts.index(a)
-            gifts[gifts.index(max(gifts))]= int((max(gifts))**0.5)
-        return sum(gifts)
+            num = math.isqrt(-heappop(gifts))
+            heappush(gifts,-num)
+        return -sum(gifts)
             
