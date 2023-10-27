e8cff89782ab6fef05b7956c6f9bdb1c5ba0d2e1
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-10-26 18:29:57 -0700

    Time: 152 ms (12.56%), Space: 16.4 MB (67.93%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,6 +1,6 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
-        for i in range(k):
+        for _ in range(k):
             # a = max(gifts)
             # index = gifts.index(a)
             gifts[gifts.index(max(gifts))]= int((max(gifts))**0.5)
