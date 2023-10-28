57ddae85878435a1887b20a786a59a6fb22a9f8a
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-10-28 14:28:21 -0700

    Time: 487 ms (99.63%), Space: 29.2 MB (88.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,12 +3,13 @@ class Solution:
         start = 0
         end = len(height)-1
         max_area = 0
-        while start < end:
+        max_height = max(height)
+        while start < end and max_height*(end-start) > max_area:
             # area = len*bred = abs(min(height[start],height[end]) * (start - end))
-            area = abs(min(height[start],height[end]) * (start - end))
-            max_area = max(area,max_area)
-            print(max_area,start,end)
-            if height[start] < height [end]:
+            max_area = max(max_area,min(height[start],height[end]) * (end - start))
+            
+            
+            if height[start] <= height [end]:
                 start +=1
             else:
                 end -= 1
