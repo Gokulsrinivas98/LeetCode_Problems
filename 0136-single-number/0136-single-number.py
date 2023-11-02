aa068d68efe3cf639f2dc84780f91df83f090037
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-01 17:17:08 -0700

    Time: 115 ms (91.96%), Space: 19 MB (29.85%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,4 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return count.most_common()[-1][0]
+        # count = Counter(nums)
+        # return count.most_common()[-1][0]
+        s = 0
+        for i in nums:
+            s = s^i
+        return s
