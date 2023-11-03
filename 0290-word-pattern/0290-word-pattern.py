bae055c9b4a40cd20f8957c4acd0ab8aba48800f
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-03 00:23:42 -0700

    Time: 39 ms (47.04%), Space: 16.3 MB (13.88%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,8 +1,8 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         l = s.split()
-        if len(pattern) != len(l):
-            return False
+        # if len(pattern) != len(l):
+        #     return False
         p_map = [pattern.index(i) for i in pattern]      
         s_map = [l.index(i) for i in l]
         return p_map == s_map
