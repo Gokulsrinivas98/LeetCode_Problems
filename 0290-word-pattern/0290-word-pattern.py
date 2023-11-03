4c98c681f1b24817bb72dffbafe86950ff9eb2a2
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-03 00:21:35 -0700

    Time: 32 ms (88.87%), Space: 16.3 MB (13.88%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -3,6 +3,4 @@ class Solution:
         p_map = [pattern.index(i) for i in pattern]
         l = list(s.split())
         s_map = [l.index(i) for i in l]
-        print(p_map)
-        print(s_map)
         return p_map == s_map
