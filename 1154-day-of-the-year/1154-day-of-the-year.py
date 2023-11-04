e8a9173fb9e07367a338adae8001f56a61a9596d
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-03 18:11:46 -0700

    Time: 67 ms (68.65%), Space: 16.2 MB (82.57%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -1,22 +1,27 @@
 class Solution:
     def dayOfYear(self, date: str) -> int:
-        def is_leap_year(year):
-              if year % 400 == 0:
-                return True
-              elif year % 100 == 0:
-                return False
-              elif year % 4 == 0:
-                return True
-              else:
-                return False
+#         def is_leap_year(year):
+#               if year % 400 == 0:
+#                 return True
+#               elif year % 100 == 0:
+#                 return False
+#               elif year % 4 == 0:
+#                 return True
+#               else:
+#                 return False
         
-        days = [31,28,31,30,31,30,31,31,30,31,30,31]
-        leap_days = [31,29,31,30,31,30,31,31,30,31,30,31]
+#         days = [31,28,31,30,31,30,31,31,30,31,30,31]
+#         leap_days = [31,29,31,30,31,30,31,31,30,31,30,31]
         
-        dates = list(map(int,date.split('-')))
-        print(dates)
-        if is_leap_year(dates[0]):
-            return sum(leap_days[0:dates[1]-1]) + dates[2]
-        else:
-            return sum(days[0:dates[1]-1]) + dates[2]
+#         dates = list(map(int,date.split('-')))
+#         print(dates)
+#         if is_leap_year(dates[0]):
+#             return sum(leap_days[0:dates[1]-1]) + dates[2]
+#         else:
+#             return sum(days[0:dates[1]-1]) + dates[2]
+        y, m, d = map(int, date.split('-'))
+        days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        if (y % 400) == 0 or ((y % 4 == 0) and (y % 100 != 0)): days[1] = 29
+        return d + sum(days[:m-1])
+
         
