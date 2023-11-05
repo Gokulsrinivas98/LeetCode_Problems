34ecf73c8e8c959b65928096df8c47335624157d
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-04 20:41:05 -0700

    Time: 536 ms (53.80%), Space: 29.7 MB (74.46%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -5,11 +5,11 @@ class Solution:
         winner = arr[0]
         cons_wins = 0
         for i in range(1,n):
-            if winner > arr[i]:
-                cons_wins += 1
-            else:
+            if winner < arr[i]:
                 winner = arr[i]
                 cons_wins = 1
+            else:
+                cons_wins += 1
             
             if cons_wins == k:
                 return winner
