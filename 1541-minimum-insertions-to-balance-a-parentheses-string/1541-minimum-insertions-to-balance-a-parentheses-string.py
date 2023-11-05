1e26739f2590cf0214b3627876eb391c8e8da192
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-04 20:56:29 -0700

    Time: 103 ms (88.10%), Space: 17.1 MB (62.78%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,16 +1,31 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        left = right = 0
+        # left = right = 0
+        # for c in s:
+        #     if c == '(':
+        #         if right % 2:
+        #             right -= 1
+        #             left += 1
+        #         right += 2
+        #     if c == ')':
+        #         right -= 1
+        #         if right < 0 :
+        #             right += 2
+        #             left += 1 
+        # return left + right
+        
+        s = s.replace('))', '}')
+        miss_bracket = req_close = 0
         for c in s:
             if c == '(':
-                if right % 2:
-                    right -= 1
-                    left += 1
-                right += 2
-            if c == ')':
-                right -= 1
-                if right < 0 :
-                    right += 2
-                    left += 1 
-        return left + right                   
+                req_close += 2
+            else:
+                if c == ')':
+                    miss_bracket += 1
+                if req_close:
+                    req_close -= 2
+                else:
+                    miss_bracket += 1
+        return req_close + miss_bracket
+                    
         
