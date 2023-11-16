cd0662767a2bef8d37970b15eb507bbd34b757eb
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-15 20:37:50 -0800

    Time: 44 ms (97.99%), Space: 17.2 MB (25.83%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,7 +1,27 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
-        s = 0
-        for i in n:
-            if int(i) > s:
-                s = int(i)
-        return s
+        # s = 0
+        # for i in n:
+        #     if int(i) > s:
+        #         s = int(i)
+        # return s
+        if '9' in n:
+            return 9
+        elif '8' in n:
+            return 8
+        elif '7' in n:
+            return 7
+        elif '6' in n:
+            return 6
+        elif '5' in n:
+            return 5
+        elif '4' in n:
+            return 4
+        elif '3' in n:
+            return 3
+        elif '2' in n:
+            return 2
+        elif '1' in n:
+            return 1
+        else:
+            return 0 
