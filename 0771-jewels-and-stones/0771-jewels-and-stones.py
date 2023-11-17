26af964acb0801aab2399c75f1ea4919ab798c27
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-11-17 09:00:01 -0800

    Time: 37 ms (70.05%), Space: 16.3 MB (31.05%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,8 +1,10 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        s = 0
-        counter = Counter(stones)
-        for i in counter:
-            if i in jewels:
-                s+=counter[i]
-        return s
+        # s = 0
+        # counter = Counter(stones)
+        # for i in counter:
+        #     if i in jewels:
+        #         s+=counter[i]
+        # return s
+        
+        return sum(s in jewels for s in stones)
