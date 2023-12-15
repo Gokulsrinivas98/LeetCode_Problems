ef241496013ff7f5bfa147a61ed1f34711f1d5c9
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-15 09:21:12 -0800

    Time: 73 ms (5.77%), Space: 16.4 MB (45.28%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,6 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        s= set(p[0] for p in paths)
+        for path in paths:
+            if path[1] not in s:
+                return path[1]
