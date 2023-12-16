93a1516daa44af6302d98a0e0d36f83c175a1d04
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-16 15:05:15 -0800

    Time: 94 ms (5.68%), Space: 16.3 MB (39.41%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        i=0
+        while i < len(words)-1:
+            if Counter(words[i]) == Counter(words[i+1]):
+                words.remove(words[i+1])
+            else:
+                i+=1
+        return words
+            
