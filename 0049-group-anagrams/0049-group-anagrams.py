173f0408126a4b6884df5c85f867446682e9bf0b
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-27 10:55:50 -0800

    Time: 79 ms (99.15%), Space: 20.4 MB (45.77%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram_map = defaultdict(list)
         for word in strs:
-            sorted_word = ''.join(sorted(word))
-            anagram_map[sorted_word].append(word)
+            
+            anagram_map[''.join(sorted(word))].append(word)
         return list(anagram_map.values())
     
