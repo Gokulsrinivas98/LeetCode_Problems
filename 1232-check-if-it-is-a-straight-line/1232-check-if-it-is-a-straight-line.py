ae41f55098869fcf14ee3af382e53294f27d8c7d
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-29 09:18:28 -0800

    Time: 110 ms (6.00%), Space: 17.8 MB (6.73%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.py
@@ -3,7 +3,7 @@ class Solution:
         x,y = arr[0][0],arr[0][1]
         dx,dy = arr[1][0]-x, arr[1][1]-y
         n = len(arr)
-        for i in range(n):
-            if dx * (arr[i][1] - y) != dy * (arr[i][0] - x):
+        for xn,yn in arr:
+            if dx * (yn - y) != dy * (xn - x):
                 return False
         return True
