6c178910c82082e04f55f2659861a510da65e38d
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-29 12:26:27 -0800

    Time: 577 ms (98.09%), Space: 17.9 MB (20.22%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,16 +1,10 @@
 class Solution:
     def checkValid(self, matrix: List[List[int]]) -> bool:
-        row = defaultdict(set)
-        col = defaultdict(set)
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                val = matrix[i][j]
-                
-                k = i//3 * 3 + j//3
-                if val in row[i] or val in col[j]:
-                    return False
-                else:
-                    row[i].add(val)
-                    col[j].add(val)
+        check = {i for i in range(1, len(matrix) + 1)}
+        for row in matrix:
+            if set(row) != check:
+                return False
+        for column in zip(*matrix):
+            if set(column) != check:
+                return False
         return True
