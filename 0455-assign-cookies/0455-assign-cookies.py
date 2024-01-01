25907e5263cb1c6afb5eadd9728c38980c775ccb
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2023-12-31 19:04:20 -0800

    Time: 134 ms (94.33%), Space: 19.3 MB (8.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,16 +1,13 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        g.sort()
-        s.sort()
-        l = len(g) - 1
-        m = len(s) - 1
-        count = 0
-        while l>-1 and m>-1:
-            if g[l]<=s[m]:
-                l-=1
-                m-=1
-                count+=1
-            else:
-                l-=1
-        return count
+        g=sorted(g, reverse=True)
+        s=sorted(s, reverse=True)
+
+        score=left=0
+        for greed in g:
+            if left<len(s) and greed<=s[left]:
+                score+=1
+                left+=1
+
+        return score
                 
