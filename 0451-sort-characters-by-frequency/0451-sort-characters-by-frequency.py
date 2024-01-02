5c5987057f2c6f94f08d04a6e33d060eff6392f3
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-02 14:43:47 -0800

    Time: 37 ms (96.36%), Space: 18.5 MB (34.85%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -1,6 +1,14 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
-        c = Counter(s)
-        s = list(s)
-        s.sort(key=lambda x:(-c[x],x))
-        return ''.join(s)
+        # c = Counter(s)
+        # s = list(s)
+        # s.sort(key=lambda x:(-c[x],x))
+        # return ''.join(s)
+        dic = Counter(s)
+        
+        f = sorted([(v,k) for k,v in dic.items()], key=lambda x:x[0], reverse=True)
+        res = ''
+        for v,k in f:
+            res+=k*v
+            
+        return res
