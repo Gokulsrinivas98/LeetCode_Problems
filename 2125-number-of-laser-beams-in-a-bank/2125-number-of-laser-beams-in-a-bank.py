04cd285155bf7e5e34d5645caddafa575be88f85
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-02 16:58:07 -0800

    Time: 166 ms (35.44%), Space: 19.6 MB (6.86%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -3,15 +3,16 @@ class Solution:
         m = len(bank)
         if m == 1: return 0
         res= 0
-        ones = []
-        for i in range(m):
-            if bank[i].count('1') > 0:
-                ones.append(bank[i].count('1'))
-        
+        # ones = []
+        # for i in range(m):
+        #     if bank[i].count('1') > 0:
+        #         ones.append(bank[i].count('1'))
+        ones = [bank[i].count('1') for i in range(m) if '1' in bank[i]]
+
         if len(ones) == 1: return 0 
-        count = 0
-        for i in range(1,len(ones)):
-            res += ones[i]*ones[i-1]
+        
+#         for i in range(1,len(ones)):
+#             res += ones[i]*ones[i-1]
             
-        return res
-                
+#         return res
+        return sum(ones[i] * ones[i-1] for i in range(1, len(ones)))      
