e1d3f8b6a96e9ba77a8dad8920c924fba2ce4706
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-02 17:01:32 -0800

    Time: 139 ms (38.40%), Space: 19.5 MB (12.99%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,18 +1,21 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        m = len(bank)
-        if m == 1: return 0
-        res= 0
-        # ones = []
-        # for i in range(m):
-        #     if bank[i].count('1') > 0:
-        #         ones.append(bank[i].count('1'))
-        ones = [bank[i].count('1') for i in range(m) if '1' in bank[i]]
+#         m = len(bank)
+#         if m == 1: return 0
+#         ones = []
+#         for i in range(m):
+#             if bank[i].count('1') > 0:
+#                 ones.append(bank[i].count('1'))
 
-        if len(ones) == 1: return 0 
+#         if len(ones) == 1: return 0 
         
 #         for i in range(1,len(ones)):
 #             res += ones[i]*ones[i-1]
             
 #         return res
+           
+        m = len(bank)
+        if m == 1: return 0
+        ones = [bank[i].count('1') for i in range(m) if '1' in bank[i]]
+        if len(ones) == 1: return 0 
         return sum(ones[i] * ones[i-1] for i in range(1, len(ones)))      
