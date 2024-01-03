fef2d9360d66c992ad178a6f48eaa3cf27fe0f88
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-02 17:06:08 -0800

    Time: 133 ms (38.59%), Space: 19.5 MB (12.99%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -14,8 +14,17 @@ class Solution:
             
 #         return res
            
-        m = len(bank)
-        if m == 1: return 0
-        ones = [bank[i].count('1') for i in range(m) if '1' in bank[i]]
-        if len(ones) == 1: return 0 
-        return sum(ones[i] * ones[i-1] for i in range(1, len(ones)))      
+        # m = len(bank)
+        # if m == 1: return 0
+        # ones = [bank[i].count('1') for i in range(m) if '1' in bank[i]]
+        # if len(ones) == 1: return 0 
+        # return sum(ones[i] * ones[i-1] for i in range(1, len(ones)))    
+        
+        floorDevs = []
+        totalBeam= 0
+        for floor in bank:
+            floorDev = floor.count("1")
+            if(floorDev>0): floorDevs.append(floorDev)
+        for i in range(len(floorDevs)-1):
+            totalBeam += floorDevs[i]*floorDevs[i+1]      
+        return totalBeam
