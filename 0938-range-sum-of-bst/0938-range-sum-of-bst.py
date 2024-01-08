004690b067d56dc1a0dec7d840ac9253635fabb8
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-07 20:20:02 -0800

    Time: 110 ms (96.77%), Space: 24.4 MB (97.66%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -7,7 +7,7 @@
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
         def rangesum(node):
-            if not node: return 0 
+            if not node: return 0
             count = 0
             if low <= node.val <= high:
                 count += node.val
@@ -15,9 +15,7 @@ class Solution:
                 count += rangesum(node.left)
             if node.val < high:
                 count += rangesum(node.right)
-            
             return count
-        
         return rangesum(root)
     
         
