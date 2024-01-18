20957ca7295d4b9332a8a29e0320b3d65c99e373
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-17 16:11:36 -0800

    Time: 29 ms (93.70%), Space: 17.2 MB (31.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        s,l = 1 , 2
+        for _ in range(2, n): 
+            # swapping `s` and `l`
+            s, l = l, s + l 
+        return l if n > 1 else s
+        
