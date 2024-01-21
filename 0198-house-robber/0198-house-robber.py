6d32164e81ed11d782328a575937f6fdad5e306d
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-20 20:07:49 -0800

    Time: 34 ms (83.66%), Space: 16.5 MB (60.22%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        prev , curr = 0,0
+        for n in nums:
+            t = prev
+            prev = curr
+            curr = max(n+t,prev)
+        return curr
+                
