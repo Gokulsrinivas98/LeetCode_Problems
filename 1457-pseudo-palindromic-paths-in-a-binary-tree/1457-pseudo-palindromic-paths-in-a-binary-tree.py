6eb308ae83933cec06d8645d85e7c8040a21a555
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-23 21:14:59 -0800

    Time: 468 ms (76.45%), Space: 62.2 MB (53.33%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        hashset = set()
+        ans = 0
+        
+        def check(node,hashset):
+            nonlocal ans
+            if not node : return
+            if node.val in hashset: hashset.remove(node.val)
+            else : hashset.add(node.val)
+            
+            if not node.left and not node.right: 
+                if len(hashset) <= 1: ans += 1
+            
+            check(node.left,hashset.copy())
+            check(node.right,hashset.copy())
+        
+        check(root,hashset)
+        return ans
