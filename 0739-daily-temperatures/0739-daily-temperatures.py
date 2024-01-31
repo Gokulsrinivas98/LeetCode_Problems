525f3c338993455200b2f227103ba940b237f9fd
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-01-30 17:47:50 -0800

    Time: 859 ms (97.37%), Space: 30.1 MB (95.84%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,31 +1,10 @@
-# class Solution:
-#     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-#         length = len(temperatures)
-#         answer = [0]*length
-#         stack = []
-#         for i in range(length):
-#             count = 0
-#             while stack and temperatures[i] > temperatures[stack[-1]]:
-#                 prev_day = stack.pop()
-#                 answer[prev_day] = i- prev_day
-#             stack.append(i)
-#         return answer
-
-f = open('user.out', 'w')
-for temperatures in stdin:
-    temperatures = loads(temperatures)
-
-    res = [0] * len(temperatures)
-    stack = []
-    
-    for day, temperature in enumerate(temperatures):
-        while stack and temperatures[stack[-1]] < temperature:
-            top_index = stack.pop()
-            res[top_index] = day - top_index
-
-        stack.append(day)
-
-
-    print("".join(['[', ','.join(str(x) for x in res), ']']), file=f)
-
-exit(0)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = [0]*len(temperatures)
+        stack = []
+        for day,temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                idx = stack.pop()
+                result[idx] = day - idx
+            stack.append(day)
+        return result
