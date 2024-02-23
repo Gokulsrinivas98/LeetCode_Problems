79b94bd070c1b75a0e5e374f93ecd388a8ad33ac
GrapeSeed <34882860+Gokulsrinivas98@users.noreply.github.com>
2024-02-23 14:51:47 -0800

    Time: 92 ms (80.90%), Space: 17.9 MB (70.50%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,9 +1,9 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         graph = collections.defaultdict(dict)
         for s, d, w in flights:
             graph[s][d] = w
-        pq = [(0, src, K+1)]
+        pq = [(0, src, k+1)]
         vis = [0] * n
         while pq:
             w, x, k = heapq.heappop(pq)
@@ -15,20 +15,3 @@ class Solution:
             for y, dw in graph[x].items():
                 heapq.heappush(pq, (w+dw, y, k-1))
         return -1
-    
-#         f = collections.defaultdict(dict)
-#         for a, b, p in flights:
-#             f[a][b] = p
-#         heap = [(0, src, k + 1)]
-      
-#         while heap:
-#             p, i, k = heapq.heappop(heap)
-#             if i == dst:
-#                 return p
-#             if k > 0:
-#                 for j in f[i]:
-#                     # print(j)/
-#                     heapq.heappush(heap, (p + f[i][j], j, k - 1))
-#                     # print (heap)
-             
-#         return -1
